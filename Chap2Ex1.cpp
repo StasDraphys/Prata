d579f6c6bc63f83f905885d75de4b015dc90f9c6
StasDraphys <44318918+StasDraphys@users.noreply.github.com>
2018-10-25 18:19:02 +0300

    Add files via upload

---
+++ Chap2Ex1.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\n";
+    cout << "      My first name: Stas;\n";
+    cout << "       My last name: Fursin;\n";
+    cout << " My primary address: Nizhegorodskaya obl, Semenov.\n";
+    cout << "\n  ";
+    system("pause");
+    return 0;
+}
